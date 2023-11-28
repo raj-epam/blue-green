2fcf914b50dbe64e7eb327ed0a8b096634f4f697
Raj Sakalabttula <raj_sakalabttula@epam.com>
2023-11-28 09:45:25 +0530

    Blue Green Deployment Demo update jenkins

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
-
 ssh -i $1 dhluser@10.0.0.5 << EOF 
 sudo ls /opt/tomcat/webapps
 sudo mkdir -p /opt/tomcat/backup/$(date +%F)/$2
 sudo cp /opt/tomcat/webapps/sample/index.html /opt/tomcat/backup/$(date +%F)/$2/
 sudo rm -f /opt/tomcat/webapps/sample/index.html
 sudo mkdir -p /opt/tomcat/webapps/sample
-chmod 777 /opt/tomcat/webapps/sample
+sudo chmod 777 /opt/tomcat/webapps/sample
 EOF
  
